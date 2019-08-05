f902a0d17e352c27e86b946c126b617de292dc5d
Hordii <hordyybohdan@gmail.com>
2019-08-05 12:51:34 +0300

    fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const serveStatic = require('serve-static')
 
 const app = express();
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(serveStatic(__dirname+'./dist'));
 
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
